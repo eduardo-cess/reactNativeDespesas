4596534512ac1585d4bc03f4ac56cf956432a47c
Eduardo Santiago <Eduardo Santiago>
2017-05-01 23:49:08 -0300

    inicio do cadastro AsyncStorage, falta cadastrar varios e recuperar as informaçoes

--- src/components/PageCadastroDespesa.js
+++ src/components/PageCadastroDespesa.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import { Text, Container, Button, Form, Content, Item, Label, Input, Toast } from 'native-base';
-import { NavigationActions } from 'react-navigation'
+import FormCadastroDespesa from './formCadastroDespesa.js';
 
 export default class PageCadastroDespesa extends Component {
     static navigationOptions = {
@@ -10,35 +10,11 @@ export default class PageCadastroDespesa extends Component {
         headerStyle: { backgroundColor: '#700769' }
     };
 
-    cadastraDespesa(){
-        // Toast.show({
-        //     text: 'Toast!',
-        //     position: 'bottom',
-        //     buttonText: 'Okay',
-        //     duration: 1000
-        // });
-        this.props.navigation.goBack();
-    }
     render() {
         return (
             <Container>
                 <Content padder>
-                    <Form>
-                        <Item floatingLabel>
-                            <Label>Descrição</Label>
-                            <Input />
-                        </Item>
-                        <Item>
-                            <Input placeholder='Alguma coisa' />
-                        </Item>
-                    </Form>
-                    <Button
-                        style={{ marginTop: 20 }}
-                        full onPress={() =>
-                           this.cadastraDespesa()
-                        }>
-                        <Text>Cadastrar Despesa</Text>
-                    </Button>
+                    <FormCadastroDespesa/>
                 </Content>
             </Container>
         );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Navigator } from 'react-native';
+import { AppRegistry, Navigator, AsyncStorage } from 'react-native';
 import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 import ListDespesas from './components/listDespesas.js';
 import App from '../index.android.js';
@@ -10,10 +10,16 @@ export default class MainView extends Component {
         headerStyle: { backgroundColor: '#700769' },
         headerLeft: (
             <Button transparent>
-                <Icon style={{color:'white'}} name='menu' />
+                <Icon style={{ color: 'white' }} name='menu' />
             </Button>
         )
     };
+
+    getDespesas() {
+        AsyncStorage.getItem('despesa', (err, result) => {
+            console.log(result);
+        });
+    }
     render() {
         const { navigate } = this.props.navigation;
         return (
@@ -26,7 +32,7 @@ export default class MainView extends Component {
                 </Content>
                 <Footer>
                     <FooterTab>
-                        <Button onPress={() => console.log('teste 123')}>
+                        <Button onPress={() => this.getDespesas()}>
                             <Text>Footer</Text>
                         </Button>
                     </FooterTab>
